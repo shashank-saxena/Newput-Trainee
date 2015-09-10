db0fc6a10cbe2109b7de1fffb7db8075a7f67969
varsha tyagi <varsha@newput.com>
2015-09-10 19:03:05 +0530

    Worked on Book specific event handler class in js , and css changes
    1) Implement the scrolling pagination in javascript class
    2) Reander the book data into a modal box.

--- javascript/templates/article-template.js
+++ javascript/templates/article-template.js
@@ -1,14 +1,16 @@
 <div id="{{id}}" class="modal-dialog">
 	<a href="#close" title="Close" class="close">X</a>
 	<div class="dialog-content">
-	<header class="title">{{Title}}</header>
-	<section>
-		<div class="book-image"><img src="{{Image}}"/></div>
-		<div class="content">{{Description}}</div>
-		<div class="short-details">
-			<div>ID: {{ID}}</div>
-			<div>ISBN : {{isbn}}</div>
-		</div>
-	</section>
-	<div>
+		<header class="title"><h3>{{Title}}</h3></header>
+		<section>
+			<div class="book-image"><img src="{{Image}}"/></div>
+			<div class="content">{{Description}}
+				<div class="short-details">
+					<div>ID: {{ID}}</div>
+					<div>ISBN : {{isbn}}</div>
+				</div>
+			</div>
+		</section>
+		<footer class="footer"><h3>footer @2015</h3></footer>
+	</div>
 </div>
--- javascript/templates/mustache-template.js
+++ javascript/templates/mustache-template.js
@@ -2,16 +2,19 @@
 	<div class='columns'>
 		<div class='cell-wrapper'>
 			<div class='ribbon'>{{isbn}}</div>
-			<div class='img'><img src='{{Image}}'/></div>
-			<div class='detail'>
+			<div class='img'>
+				<img src='{{Image}}'/>
 				<div class='short-detail'>
-				<div class='title'><a class="more-detail" id="title-{{id}}" href="#{{id}}">{{Title}}</a></div>
-				<div class='author'>by Author</div>
+					<div class='title'><a class="more-detail" id="title-{{id}}" href="#{{id}}">{{Title}}</a></div>
+					<div class='author'>by Author</div>
+					<div class='description'>{{shortDescription}}...</div>
+				</div>
 			</div>
-			<div class='description'>{{shortDescription}}...</div>
-		</div>
-		<div class='link'><a href='#{{id}}' class="more-detail" id="book-{{id}}">Learn More -></a></div>
+		    <div class='link'><a href='#{{id}}' class="more-detail" id="book-{{id}}">Learn More -></a></div>
 		</div>
 	</div>
 {{/Books}}
-<div class="clear"></div>
+{{^Books}}
+  No More Books are available !!
+{{/Books}}
+
