dd5634f7b1d31e0e5b99e72780a739786bdf9922
deepak patidar <deepak@newput.com>
2015-09-14 10:54:58 +0530

    Changes after os update
    Contains changes of js and html

--- deepak_practice/js/creditcardform.js
+++ deepak_practice/js/creditcardform.js
@@ -1,15 +1,25 @@
 	        function creditcardWraper() {        	
 	        	
 	        	$('.credit-card').validateCreditCard(function(result) {
-		            $('.log').html('Valid: ' + result.valid);
+		            //$('.log').html('Valid: ' + result.valid);
+	        		creditCardCallback(result);
 		                    
 		        });
-	
+	        	
+	        	$.validator.addMethod("fullname", function(value, element) {	        		 
+	        		return /\s/.test(value); 
+	        	}, "Please enter full name");
+	        	 
+	            $.validator.addMethod("dateHigherThanToday", function(value, element) {        		        		
+	        		var myDate = value;	        		
+	        		return Date.parse(myDate) > new Date();	    
+	        	}, "Date must be higher than current date"); 
+	        	 
 	    		// validate signup form on keyup and submit
-	    		$("#commentForm").validate({
+	    		$("#commentForm").validate({ 
 	    			rules: {
-	    				firstname: {
-	    					required: true,
+	    				fullname: {
+	    					fullname: true,
 	    					minlength: 2,
 	    				},
 	    				email: {
@@ -20,7 +30,9 @@
 	    					required: true,
 	    					minlength: 10,
 	    				},
-	    				expirationDate: "required",
+	    				expirationDate: {
+	    					dateHigherThanToday: true,	
+	    				},
 	    				cvs: "required",
 	    				street: "required",
 	    				city: "required",
@@ -28,22 +40,28 @@
 	    				country: "required",
 	    			},
 	    			messages: {
-	    				firstname: "Please enter your Firstname",
+	    				fullname: "Please enter your Firstname",
 	    				email: "Please enter a valid email address",
 	    				creditcard: "Please enter a valid credit card number",
-	    				expirationDate: "Please select Expiration Date",
 	    				cvs: "Please enter CVS",
 	    				city: "Please enter City",
 	    				state: "Please enter State",
 	    				country: "Please enter Country",
 	    			}
-	    		});
+	    			});
 	    		
 	    		$("#street").geocomplete({	
 					  details: ".geo-details",
 					  detailsAttribute: "data-geo"
 					 
 				});
+	    		
+	    		 function creditCardCallback(result) {
+	    			var templete = $('#creditcard-callback').html();
+	    			//console.log(result);
+	    			var html = Mustache.to_html(templete,result);	    			
+	    			$(".log").html(html);
+	    		}
 	    		
 	    		$('#expirationdate').datepick();	
 	        };		
